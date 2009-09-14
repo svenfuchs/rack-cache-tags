c449b5da6c7eea8564e65a3f058080e90b0135a7
Sven Fuchs <svenfuchs@artweb-design.de>
2009-09-14 17:35:24 +0200

    make Storage#clear clear the tagstores as well

--- lib/rack/cache/tags/storage.rb
+++ lib/rack/cache/tags/storage.rb
@@ -1,4 +1,10 @@
 Rack::Cache::Storage.class_eval do
+  alias :clear_without_tagstore :clear
+  def clear
+    @tagstores.clear
+    clear_without_tagstore
+  end
+
   def resolve_tagstore_uri(uri)
     @tagstores ||= {}
     @tagstores[uri.to_s] ||= create_store(Rack::Cache::Tags::TagStore, uri)
